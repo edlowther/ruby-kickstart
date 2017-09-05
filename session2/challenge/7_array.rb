e160e83bff0b4c440e9ca7a44384f44436f3335a
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-05 21:29:14 +0100

    Seventh challenge done

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,21 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
-# alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
-# alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
+
+def alternate_words(sentence)
+  punctuation = ["!","@","$","#","%","^","&","*","(",")","-","=","_","+","[","]",":",";",",",".","/","<",">","?","\\","\|"]
+  punctuation.each do |char|
+    sentence = sentence.gsub(char, " ")
+  end
+  sentence = sentence.split(" ")
+  sentence = sentence.select.with_index do | x, i |
+    i.even?
+  end
+  sentence
+end
+
+# alternate_words("If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic.")  # => ["Lorem", "dolor", "amet"]
+# alternate_words("Can't we all get al^ong?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
