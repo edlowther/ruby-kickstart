1f9320d621371e30e84d35234d685516ea2bfb46
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-05 11:15:27 +0100

    Challenge seven done

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  result = ""
+  string.each_char.with_index() do |char, i|
+    if char.downcase == "r" and i < string.length - 1
+      result += string[i + 1]
+    end
+  end
+  result
 end
