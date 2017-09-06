f81fc17d920dd5727c46b47828f1d90a475d586c
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-06 10:35:38 +0100

    11 done

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,57 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+  def initialize number_of_bottles
+    if number_of_bottles < 0
+      @number_of_bottles = 0
+    elsif number_of_bottles > 99
+      @number_of_bottles = 99
+    else
+      @number_of_bottles = number_of_bottles
+    end
+  end
+
+  def convert_to_english(number)
+    below_ten = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    teens = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+    prefixes = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    suffixes = ["", "-one", "-two", "-three", "-four", "-five", "-six", "-seven", "-eight", "-nine"]
+    if number < 10
+      if number == 1
+        return below_ten[number] + " bottle "
+      else
+        return below_ten[number] + " bottles "
+      end
+    elsif number < 20
+      return teens[number % 10] + " bottles "
+    else
+      return prefixes[number / 10 - 2] + suffixes[number % 10] + " bottles "
+    end
+    number.to_s
+  end
+
+
+  def print_song
+    output = ""
+    @number_of_bottles.downto(1) do |i|
+      output += convert_to_english(i) + "of beer on the wall,\n"
+      output += convert_to_english(i)+ "of beer,\n"
+      output += "Take one down, pass it around,\n"
+      output += convert_to_english(i - 1) + "of beer on the wall.\n"
+    end
+    puts output
+  end
+
+end
+
+beerSong = BeerSong.new(108)
+beerSong.print_song()
