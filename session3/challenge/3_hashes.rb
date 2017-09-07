2c880be185fc02ff6579a01985b2f9ca5d4a1241
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-07 09:36:47 +0100

    Three done

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+  result = Hash.new { |this_hash, key| this_hash[key] = 0 }
+  if string.length > 0
+    string.downcase!.split(" ").each do |word|
+      result[word] += 1
+    end
+  end
+  result
+end
