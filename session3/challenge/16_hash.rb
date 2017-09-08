ada9988b981a970be8451bf45fdfca2a3658ea51
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-08 22:36:52 +0100

    16 done

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,27 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list(linked_list)
+  def data_grabber(linked_list, output = "")
+    if !linked_list[:next]
+      output += linked_list[:data].to_s + "\n"
+    else
+      output += linked_list[:data].to_s + "\n"
+      output = data_grabber(linked_list[:next], output)
+    end
+    print output
+  end
+  data_grabber(linked_list)
+end
+# head = {:data => 1, :next => nil}
+# print_list head                    # >> "1\n"
+# head = {:data => 2, :next => head}
+# print_list head                    # >> "2\n1\n"
+# head = {:data => 3, :next => head}
+# print_list head                    # >> "3\n2\n1\n"
+# head = {:data => 4, :next => head}
+# print_list head                    # >> "4\n3\n2\n1\n"
+# head = {:data => 5, :next => head}
+# print_list head                    # >> "5\n4\n3\n2\n1\n"
+# head = {:data => 6, :next => head}
+# print_list head
