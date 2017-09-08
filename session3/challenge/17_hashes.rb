d6a7e6a9b070c6d392c11400f8dfd783092993ff
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-08 22:38:57 +0100

    17 done

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,15 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+def print_list_in_reverse(linked_list)
+  def data_grabber(linked_list, output = "")
+    if !linked_list[:next]
+      output = linked_list[:data].to_s + "\n" + output
+    else
+      output = linked_list[:data].to_s + "\n" + output
+      output = data_grabber(linked_list[:next], output)
+    end
+    print output
+  end
+  data_grabber(linked_list)
+end
